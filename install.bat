98e3f24532ca9b44afb0997e7e8bb8ba4ddcb344
toplefty <rubrik10@gmail.com>
2015-06-21 21:25:21 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @ECHO off
-ECHO Installation Newsletter Builder 0.9.0
-PAUSE
+ECHO Newsletter Builder 0.9.0
+ECHO Press any key to continue or Ctrl-C to abort...
+PAUSE > nul
 CALL npm install
 CALL bower install
 CALL gulp install
